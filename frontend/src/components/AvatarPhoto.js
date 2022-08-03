8705e4027079aeb8d0a0fdd3e2cfd5522530e1c1
Gilad Danino <daninog6@gmail.com>
2022-08-03 18:19:20 +0300

    Added user logout button
    Appears when clicking on the user avatar

--- frontend/src/components/AvatarPhoto.js
+++ frontend/src/components/AvatarPhoto.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export const AvatarPhoto = () => {
     const [avatarUrl, setAvatarUrl] = React.useState('');
+    const [anchorMenu, setAnchorMenu] = React.useState(null);
+    const isMenuOpen = Boolean(anchorMenu);
 
     const fetchUserAvatarUrl = async() =>{
         try {
@@ -23,12 +27,36 @@ export const AvatarPhoto = () => {
         }
     }
 
+    const openMenu = (event) => {
+        setAnchorMenu(event.currentTarget);
+      };
+
+    const closeMenu = () => {
+        setAnchorMenu(null);
+    };
+
+    const handleLogout = () => {
+        sessionStorage.clear();  
+        window.location = "/";
+    }
+
     React.useEffect(async () => {
         const avatarUrlRes = await fetchUserAvatarUrl();
         setAvatarUrl(avatarUrlRes);
     });
 
     return (
-        <Avatar src={avatarUrl} />
+        <>
+            <Avatar src={avatarUrl} onClick={openMenu} />
+            <Menu
+            id="basic-menu"
+            anchorEl={anchorMenu}
+            open={isMenuOpen}
+            onClose={closeMenu}
+            MenuListProps={{'aria-labelledby': 'basic-button'}}
+            >
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+      </>
     )
 }
